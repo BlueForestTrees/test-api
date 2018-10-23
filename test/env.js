e1323d1b417a1254398043a34f1852f4ba9adfaf
slim <smedini@gmail.com>
2018-10-23 10:16:41 +0200

    con string

--- test/env.js
+++ test/env.js
@@ -1,4 +1,5 @@
 const ENV = {
+    DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
     DB_NAME: process.env.DB_NAME || "BlueForestTreesDB",
     DB_HOST: process.env.DB_HOST || "localhost",
     DB_PORT: process.env.DB_PORT || 27017,
